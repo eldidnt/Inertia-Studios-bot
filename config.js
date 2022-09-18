a1608abe156d27ddf714788843f4f4ea2f5f7c5a
Robot <luiscervantes.pe@gmail.com>
2022-09-18 10:15:45 -0500

    verify footer fixed

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 module.exports = {
     token: "OTkxODcxMDIwMjEyNDIwNjg4.GlmULk.I8SnuOANvtx58Tb9BLwYlaRtVZgVg0ZNOWLQ9M",
     prefix: "!",
+    embedFooterText: "Inertia Studios",
     color: "#3E3E3E",
     errorColor: "#FF3A3A",
     channels: {
